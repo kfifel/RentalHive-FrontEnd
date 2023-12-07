31c3ddf93835b3931efb7763ae767499c6ca1114
khalid fifel <fifle.khalid99@gmail.com>
2023-12-07 11:37:02 +0100

    update order list

--- src/app/dashboards/frontoffice/order/order-list/order-list.component.ts
+++ src/app/dashboards/frontoffice/order/order-list/order-list.component.ts
@@ -1,65 +1,96 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
-import {EquipmentService} from "../../../backoffice/equipment/service/equipment.service";
-import {DatePipe} from "@angular/common";
-import {Equipment} from "../../../backoffice/equipment/equipment.model";
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
 
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+/** Constants used to fill up our data base. */
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
-export class OrderListComponent implements OnInit {
-    firstFormGroup: FormGroup = this._formBuilder.group({
-      startDate: [null, Validators.required],
-      endDate: [null, Validators.required]
-    });
-    secondFormGroup: FormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
-    });
-    startDate: Date | null = null;
-    endDate: Date | null = null;
-    equipmentsAvailable: Equipment[] = [];
-    constructor(private _formBuilder: FormBuilder,
-                private equipmentService: EquipmentService,
-                private datePipe: DatePipe
-    ) { }
+export class OrderListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<UserData>;
 
-    ngOnInit(): void {
-    }
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+  @ViewChild(MatSort) sort?: MatSort;
 
-  fetchEquipmentAvailable() {
-    if(this.startDate === null || this.endDate === null){
-      alert("Please select a date range");
-      return;
-    }
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
-    const isoStartDate = this.datePipe.transform(this.startDate, 'yyyy-MM-ddTHH:mm:ss');
-    const isoEndDate = this.datePipe.transform(this.endDate, 'yyyy-MM-ddTHH:mm:ss');
-
-    if(isoEndDate === null || isoStartDate === null){
-      alert("Some things went wrong");
-      return;
-    }
-
-    this.equipmentService.available({startDate: isoStartDate, endDate: isoEndDate})
-      .subscribe({
-        next: (response) => {
-          if(response.result)
-            this.equipmentsAvailable = response.result;
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      });
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
-  updateStartDate(event: any): void {
-    this.firstFormGroup.get('startDate')?.setValue(event);
+  ngAfterViewInit() {
+    if(this.paginator)
+    this.dataSource.paginator = this.paginator;
+    if(this.sort)
+      this.dataSource.sort = this.sort;
   }
 
-  updateEndDate(event: any) {
-    this.firstFormGroup.get('endDate')?.setValue(event);
-    console.log(event)
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
 }
